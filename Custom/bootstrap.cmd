b5a8a2977003509e200dfcecd025492f7146bfdf
anvilportal1 <anvilportal1@users.noreply.github.com>
2023-10-07 10:33:00 -0400

    added bootstrap and material compilers

---
+++ Custom/bootstrap.cmd
@@ -0,0 +1,6 @@
+set src=C:\git\2023_09_23\Solutions\Anvil-Kendo-Themes\packages\bootstrap\scss\all.scss
+set trg=C:\git\2023_09_23\Solutions\Anvil-Portal-Security\Anvil.Portal.Security.Web\wwwroot\css\bootstrap.css
+
+sass %src% %trg%
+
+pause
---
+++ Custom/bootstrap.cmd.bak
@@ -0,0 +1,6 @@
+set src=C:\git\2023_09_23\Solutions\Anvil-Kendo-Themes\packages\fluent\scss\all.scss
+set trg=C:\git\2023_09_23\Solutions\Anvil-Kendo-Themes\Custom\fluent.css
+
+sass %src% %trg%
+
+pause
