c9d7c62b84dd560c3b8e35139e4b28f4cf9ffd67
anvilportal1 <anvilportal1@users.noreply.github.com>
2023-10-07 21:02:19 -0400

    modified bootstrap output path

--- Custom/bootstrap.cmd
+++ Custom/bootstrap.cmd
@@ -1,6 +1,9 @@
-set src=C:\git\2023_09_23\Solutions\Anvil-Kendo-Themes\packages\bootstrap\scss\all.scss
+cd C:\git\2023_09_23\Solutions\Anvil-Kendo-Themes
+
+set src=C:\git\2023_09_23\Solutions\Anvil-Kendo-Themes\node_modules\bootstrap\scss\bootstrap.scss
 set trg=C:\git\2023_09_23\Solutions\Anvil-Portal-Security\Anvil.Portal.Security.Web\wwwroot\css\bootstrap.css
 
 sass %src% %trg%
+
 
 pause
--- sass.config.js
+++ sass.config.js
@@ -3,38 +3,38 @@ module.exports = {
         'sass-build:recommended'
     ],
     build: [
-        {
-            cwd: 'packages/default',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
+        //{
+        //    cwd: 'packages/default',
+        //    file: 'scss/all.scss',
+        //    outFile: 'dist/all.css'
+        //},
         {
             cwd: 'packages/bootstrap',
             file: 'scss/all.scss',
-            outFile: 'dist/all.css'
+            outFile: '/git/2023_09_23/Solutions/Anvil-Portal-Security/Anvil.Portal.Security.Web/wwwroot/css/themes/bootstrap/kendo-bootstrap.css'
         },
-        {
-            cwd: 'packages/classic',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/material',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/nouvelle',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/fluent',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css',
-            compiler: 'sass',
-            api: 'modern'
-        },
+        //{
+        //    cwd: 'packages/classic',
+        //    file: 'scss/all.scss',
+        //    outFile: 'dist/all.css'
+        //},
+        //{
+        //    cwd: 'packages/material',
+        //    file: 'scss/all.scss',
+        //    outFile: 'dist/all.css'
+        //},
+        //{
+        //    cwd: 'packages/nouvelle',
+        //    file: 'scss/all.scss',
+        //    outFile: 'dist/all.css'
+        //},
+        //{
+        //    cwd: 'packages/fluent',
+        //    file: 'scss/all.scss',
+        //    outFile: 'dist/all.css',
+        //    compiler: 'sass',
+        //    api: 'modern'
+        //},
         {
             cwd: 'packages/core',
             file: 'scss/all.scss',
